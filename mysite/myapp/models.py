80fb2aeb44cd987a7a6d0cbafcd0abb11b81f0b8
Rabiul Hasan <rabiulhasanriaz@gmail.com>
2022-05-09 12:54:31 +0200

    third

--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -3,5 +3,8 @@ from django.forms import CharField, IntegerField
 
 # Create your models here.
 class First(models.Model):
-    name = CharField(max_length=50)
-    mobile = IntegerField()
+    firstname = CharField(max_length=50)
+    lastname = CharField(max_length=50)
+    in_no = IntegerField()
+    class Meta:
+        db_table = 'myapp_first'
--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
+from .models import First
+from pprint import pprint
 
 # Create your views here.
 
 def add(request):
+    # pprint(request.POST.get('lastname'))
+    if request.method == 'POST':
+    #     # pprint('a')
+    #     # firstname = request.POST.get('firstname','')
+    #     # # pprint(firstname)
+    #     # lastname = request.POST.get('lastname','')
+    #     # in_no = request.POST.get('in_no','')
+
+        # first = First()
+        # first.firstname = request.POST.get('firstname')
+        # first.lastname = request.POST.get('lastname')
+        # first.in_no = 123
+        # first.save()
+        
+        
+        first = First(firstname=request.POST.get('firstname'),lastname=request.POST.get('lastname'),in_no=123)
+        first.save()
     return render(request, 'myapp/add.html')
